82c722ec4f488fdf67552750280493bdf1ddab8b
ExposureExplorers <exposure.explorers@nitgoa.ac.in>
2026-09-14 04:51:56 +0530

    fixed merch load screen

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function InnerApp() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSplash(false);
-    }, 500); // 3.5 seconds
+    }, 3500); // 3.5 seconds
 
     return () => clearTimeout(timer);
   }, []);
